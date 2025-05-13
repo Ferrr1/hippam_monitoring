349febde23c4f20f898691f88cc3e35e13e0b26b
Maulana Feri Setyawan <maulanasetyawan8@gmail.com>
2025-05-13 15:15:59 +0700

    add: default value status

--- database/migrations/2025_04_12_134000_create_devices_table.php
+++ database/migrations/2025_04_12_134000_create_devices_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
             $table->string('device_id')->unique();
-            $table->enum('status', ['aktif', 'tidak_aktif']);
+            $table->enum('status', ['aktif', 'tidak_aktif'])->default('tidak_aktif');
             $table->timestamp('last_seen_at')->nullable();
             $table->timestamps();
         });
